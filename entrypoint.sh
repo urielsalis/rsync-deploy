4a4158f9c860920ec9483bd4bbdd32538fd01540
urielsalis <uriel@urielsalis.me>
2023-02-02 22:28:54 +0100

    Add sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,5 +12,6 @@ SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
 az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
 # Open tunnel
 az network bastion tunnel --port 50022 --resource-port 22 --target-resource-id $RESOURCE_ID --name $BASTION_NAME --resource-group $RESOURCE_GROUP &
+sleep 5
 #Sync it
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
