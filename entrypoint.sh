9499f00264a027f1ed60bdef00756200a9201562
Uriel Salischiker <uriel@urielsalis.me>
2023-02-11 16:42:57 +0100

    Add more sleep

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,6 @@ az config set extension.use_dynamic_install=yes_without_prompt
 az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
 # Open tunnel
 az network bastion tunnel --port 50022 --resource-port 22 --target-resource-id $RESOURCE_ID --name $BASTION_NAME --resource-group $RESOURCE_GROUP &
-sleep 5
+sleep 60
 #Sync it
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
