5b6c5b8d769928c53b9fba8ce87ed791049eacf4
Uriel Salischiker <uriel@urielsalis.me>
2023-02-11 16:33:09 +0100

    Allow azure to install bastion without prompt

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ chmod 600 "$SSHPATH/key"
 SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
 
 # Login to Azure
+az config set extension.use_dynamic_install=yes_without_prompt
 az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
 # Open tunnel
 az network bastion tunnel --port 50022 --resource-port 22 --target-resource-id $RESOURCE_ID --name $BASTION_NAME --resource-group $RESOURCE_GROUP &
