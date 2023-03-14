a7b4b9379386600459fa5fa30903ef68fd4b6478
violine1101 <violine1101@gmail.com>
2023-03-14 13:48:32 +0100

    Wait for bastion port to open instead of 60s delay

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,19 @@ echo "$DEPLOY_KEY" > "$SSHPATH/key"
 chmod 600 "$SSHPATH/key"
 SERVER_DEPLOY_STRING="$USERNAME@$SERVER_IP:$SERVER_DESTINATION"
 
-# Login to Azure
+echo Login to Azure
 az config set extension.use_dynamic_install=yes_without_prompt
 az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
-# Open tunnel
+
+echo Opening tunnel
 az network bastion tunnel --port 50022 --resource-port 22 --target-resource-id $RESOURCE_ID --name $BASTION_NAME --resource-group $RESOURCE_GROUP &
-sleep 60
-#Sync it
+# Wait for bastion tunnel port to open
+{
+  while ! echo -n > /dev/tcp/localhost/50022; do
+    sleep 1
+  done
+  sleep 1
+} 2>/dev/null
+
+echo Synchronizing
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $SERVER_PORT' $GITHUB_WORKSPACE/$FOLDER $SERVER_DEPLOY_STRING"
