ea72300de7d615e9bf74747d66a6545b27d469d1
violine1101 <violine1101@gmail.com>
2024-07-20 16:20:39 +0200

    Readd bastion running in background
    I don't think it'd work without this

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ az config set extension.use_dynamic_install=yes_without_prompt
 az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET --tenant $TENANT_ID
 
 echo Opening tunnel
-az network bastion tunnel --port 50022 --resource-port 22 --target-resource-id $RESOURCE_ID --name $BASTION_NAME --resource-group $RESOURCE_GROUP
+az network bastion tunnel --port 50022 --resource-port 22 --target-resource-id $RESOURCE_ID --name $BASTION_NAME --resource-group $RESOURCE_GROUP &
 
 echo Wait for bastion tunnel port to open
 sleep 60
